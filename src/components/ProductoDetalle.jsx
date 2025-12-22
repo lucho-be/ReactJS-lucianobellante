4ea392ab8c4db93e80dc5a1db0173bec924855a2
Luciano <luciano161998@gmail.com>
2025-12-22 13:40:44 -0300

    producto detalle

--- src/components/ProductoDetalle.jsx
+++ src/components/ProductoDetalle.jsx
@@ -1,7 +1,20 @@
 export default function ProductoDetalle() {
   return (
     <div className="productDetail">
-        
+        <div>
+        <h1>Producto</h1>
+        <img src="public\img\productos\Jarrón Cilíndrico.jpg" alt="" />
+        </div>
+        <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis modi natus odit assumenda molestiae sunt eligendi possimus inventore, maxime sit excepturi non eius totam vero delectus deserunt quasi quod officia!
+        </p>
+        <div>
+        <input type="button" value="+" />
+        <input type="number" name="" id="" />
+        <input type="button" value="-" />
+        </div>
+        <button>Agregar al carrito</button>
+        </div>
     </div>
 
   )
